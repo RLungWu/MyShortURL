3a01a68c0b9a0a86428423cd0cee909620ea2acc
RLungWu <77057937+RLungWu@users.noreply.github.com>
2024-04-21 23:01:52 +0800

    Database Part Complete database.go is used to create a redis database

--- cmd/MyShortURL/main.go
+++ cmd/MyShortURL/main.go
@@ -5,24 +5,33 @@ import (
 	"log"
 	"os"
 
+	"github.com/RLungWu/MyShortURL/routes"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func setUpRoutes(app *gin.Engine) {
-	app.POST("/api/v1", routes.CreateShortURL)
-	app.GET("/:shortURL", routes.ResolveURL)
+	app.POST("/api/v1", routes.CreateShortURL())
+	app.GET("/:shortURL", routes.ResolveURL())
 }
 
 func main() {
-	err := godotenv.Load()
+	err := godotenv.Load("../../.env")
 	if err != nil {
-		fmt.Println("Error loading .env file")
+		fmt.Println("Error loading .env file:", err)
 	}
 
 	app := gin.Default()
 	app.Use(gin.Logger())
 
 	setUpRoutes(app)
-	log.Fatal(app.RunListener(os.Getenv("PORT")))
+
+	port := os.Getenv("PORT")
+	if port == ""{
+		log.Fatal("PORT must be set in .env file")
+	}
+
+	log.Fatal(app.Run(":" + port))
+
 }
--- db/database.go
+++ db/database.go
@@ -1,2 +1,21 @@
 package db
 
+import (
+	"context"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var Context = context.Background()
+
+func CreateClient(dbno int) {
+	// CreateClient creates a new client
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB: dbno,
+	})
+
+	return rdb
+}
