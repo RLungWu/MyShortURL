9fd47263a4eb93eea8c9b72b61c7c06b930880ff
RLungWu <77057937+RLungWu@users.noreply.github.com>
2024-04-21 23:31:29 +0800

    This part of code is to resolve the URL and direct to the correct website

--- routes/resolve.go
+++ routes/resolve.go
@@ -1,6 +1,34 @@
 package routes
 
+import (
+	"net/http"
+
+	"github.com/RLungWu/MyShortURL/db"
+	"github.com/redis/go-redis/v9"
+
+	"github.com/gin-gonic/gin"
+)
+
 func ResolveURL(c *gin.Context) {
 	// ResolveURL is the handler for GET /:shortURL
 	// It resolves a short URL to the original URL
+	shortURL := c.Param("shorturl")
+	
+	r := db.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(db.Context, shortURL).Result()
+	if err != redis.Nil{
+		c.JSON(http.StatusNotFound, gin.H{"error" : "URL not found in database!"})
+		return
+	}else if err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error" : "Cannot connect to database!"})
+	}
+
+	rInr := db.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(db.Context, "counter")
+
+	c.Redirect(http.StatusMovedPermanently, value)
 }
